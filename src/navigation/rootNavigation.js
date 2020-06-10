81d77bbc1606e0c4d42e10e2d7b3114b3108ecc9
AminHussaini <59252100+AminHussaini@users.noreply.github.com>
2020-06-10 13:34:22 +0500

    Initial Push
    Design Completed

--- src/navigation/rootNavigation.js
+++ src/navigation/rootNavigation.js
@@ -1,27 +1,41 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-import { LoginContainer, CreateAccountContainer } from '../screens';
+import {
+  LoginContainer,
+  CreateAccountContainer,
+  WelcomeScreen,
+} from '../screens';
 
-const AppNavigator = createStackNavigator({
+const AppNavigator = createStackNavigator(
+  {
+    Welcome: {
+      screen: WelcomeScreen,
+    },
     Login: {
       screen: LoginContainer,
     },
     CreateAccount: {
-        screen: CreateAccountContainer,
-        navigationOptions: {
-            headerTitle: 'Create Account'
-        }
-    }
-  }, {
-      defaultNavigationOptions: ({navigation}) => {
-          return {
-              headerTitle: navigation.state.routeName
-          }
-      }
-  });
-  
-  const AppContainer = createAppContainer(AppNavigator);
+      screen: CreateAccountContainer,
+      navigationOptions: {
+        headerTitle: 'Create Account',
+      },
+    },
+  },
+  {
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    },
 
-  export default AppContainer;
-  
+    // defaultNavigationOptions: ({navigation}) => {
+    //   return {
+    //     headerTitle: navigation.state.routeName,
+    //   };
+    // },
+  },
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default AppContainer;
